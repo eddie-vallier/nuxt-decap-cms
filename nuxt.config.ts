b3958aa02ae1a3c8ae7945a5d1daea960e645ad7
Eddie Vallier <32486404+H4KRO@users.noreply.github.com>
2025-05-21 00:45:08 +0200

    First commmit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui"
+  ]
+})
