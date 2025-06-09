7d8a9e9f79cb1fbf1695943bd2d50b4257d1b525
EddieVallier <eddie.vallier@gmail.com>
2025-06-09 23:04:59 +0200

    Try to fix

--- src/types.ts
+++ src/types.ts
@@ -34,7 +34,7 @@ const backendSchema = z.object({
   name: z.string(),
   repo: z.string(),
   branch: z.string().default('main'),
-  site_url: z.string().optional(),
+  site_url: z.string().default(''),
 })
 
 export const moduleOptionsSchema = z.object({
